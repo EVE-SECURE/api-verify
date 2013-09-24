15dbccb370cb9dee6bf30349a0ad47590fc0c301
matthew.a.crist@gmail.com <matthew.a.crist@gmail.com@1ac92d49-2286-5328-3e40-1c1ae3015a43>
2013-09-24 20:19:15 +0000

    Updated.  Changed the cache table to pull out of date caches from current timestamp.

--- trunk/sql/characters.sql
+++ trunk/sql/characters.sql
@@ -4,7 +4,6 @@ create table characters(
 	`race`       varchar(255) not null default '', 
 	`gender`     varchar(32)  not null default '', 
 	`user`       int(10)      not null, 
-	`cacheUntil` timestamp    not null,
 	primary key(`id`), 
 	unique  key(`name`), 
    foreign key(`user`) references `users`(`id`) on delete cascade
--- trunk/sql/corporations.sql
+++ trunk/sql/corporations.sql
@@ -1,7 +1,6 @@
 create table `corporations` (
 	`id` int(17) not null, 
 	`name` varchar(124) not null default '', 
-	`cacheUntil` timestamp not null, 
 	`standing` float not null default 0.0, 
 	primary key(`id`), 
 	unique key(`name`)
